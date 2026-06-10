5f426019402bb208609254001effea1519467814
Sanzhen Liu <liu3zhen@ksu.edu>
2026-06-10 11:47:43 -0500

    update

--- RNA-Seq-Workshop/2026_labs/lab_DE/data/rc.sub.sh
+++
@@ -1,2 +0,0 @@
-cut -f 1,2-4,6-8 rc.raw.txt | head -n 20000 > rc.txt
-
--- RNA-Seq-Workshop/2026_labs/lab_DE/html2pdf.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-decktape --chrome-arg=--allow-file-access-from-files --size 1024x768 RNASeq-DE.html RNASeq-DE.pdf 
